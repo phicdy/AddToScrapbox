50aa1a13e4af66b991998dc9bbab723d769d5615
Keisuke Yamaguchi <phicdy@gmail.com>
2020-05-13 21:43:44 +0900

    Main: Fetch title from URL

--- main/src/main/java/com/phicdy/addtoscrapbox/main/MainActionCreator.kt
+++ main/src/main/java/com/phicdy/addtoscrapbox/main/MainActionCreator.kt
@@ -1,17 +1,25 @@
 package com.phicdy.addtoscrapbox.main
 
 import android.net.Uri
+import android.util.Log
 import com.phicdy.addtoscrapbox.core.Dispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
+import java.io.IOException
 
 class MainActionCreator(
     private val dispatcher: Dispatcher
 ) {
     suspend fun execute(projectUrl: String, input: String) {
         withContext(Dispatchers.IO) {
-            val uri = Uri.parse("$projectUrl$input")
-            dispatcher.dispatch(MainAction(MainState.Loaded(uri)))
+            try {
+                val doc = Jsoup.connect(input).get()
+                val result = doc.title()
+                val uri = Uri.parse("$projectUrl$result?body=$input")
+                dispatcher.dispatch(MainAction(MainState.Loaded(uri)))
+            } catch (e: IOException) {
+            }
         }
     }
 }
